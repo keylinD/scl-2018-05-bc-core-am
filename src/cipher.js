6c29b2c0dbaf57c8a211dd5ccb230a0237e5e049
Keylin_D <keylin.05.01@gmail.com>
2018-05-31 12:32:47 -0400

    Por fin decifra

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,46 @@
 window.cipher = {
-  encode: () => {
+  encode: (offset,stringCode) => {
     /* Acá va tu código */
+    let result = ""
+    
+    for (var i = 0; i < stringCode.length; i++) {
+      
+      let toasciiCode = stringCode.charCodeAt(i);
+      
+    if (64 <= toasciiCode && toasciiCode < 90) {
+      result += String.fromCharCode((toasciiCode - 65 + parseInt(offset))% 26 + 65);
+      
+      }else if ( 97 <= toasciiCode   && toasciiCode < 122) {
+        result += String.fromCharCode((toasciiCode - 97 + parseInt(offset))% 26 + 97);
+      }else{
+        //result +=stringCode.charAt(i);
+        result += String.fromCharCode(toasciiCode);
+      }
+     
+      
+    }
+    return result;
   },
-  decode: () => {
+  
+  decode: (offset,stringEncode) => {
     /* Acá va tu código */
   }
 }
+
+
+
+   
+  /* for (var i = 0; i < message.length; i++)  {// for (inicio; condición de parada; incrementador)
+     let toAsciiCode = message.charCodeAt(i); // determinar el caracter en código ascii
+ 
+     if(toAsciiCode > 64 && toAsciiCode < 91){      //condicion para mayúsculas
+      result += String.fromCharCode((toAsciiCode - 65 + parseInt(offset)) % 26 + 65);
+     } else if(toAsciiCode > 96 && toAsciiCode < 123){  //condición para minúsculas
+      result += String.fromCharCode((toAsciiCode - 97 + parseInt(offset)) % 26 + 97);
+     } else
+      result += String.fromCharCode(toAsciiCode);  //todos los demás caracteres
+    }
+   return result;
+   
+  }
+ }*/
--- src/index.js
+++ src/index.js
@@ -1,4 +1,42 @@
 /* Acá va tu código */
-function EliminarTexto(){
+/*function EliminarTexto(){
 document.getElementById("reset").value =document.getElementById("text1").value="" ;
+}*/
+window.onload = () => {
+  const code = document.getElementById("code");
+ 
+  code.addEventListener("click", () => {
+    let stringCode = document.getElementById('textEncryption').value;
+    let offset = document.getElementById('offset').value;
+    console.log(offset);
+    let result = window.cipher.encode(offset, stringCode);
+    document.getElementById("result").innerHTML = result;
+    console.log(result);
+    }); 
+ };
+
+
+
+
+
+
+/*
+window.onload = () =>{
+  const  code = document.getElementById('code');
+  const decipher = document.getElementById('decipher');
 }
+
+code.addEventListener('click' , () =>{
+  let stringCode = document.getElementById('textEncryption');
+  let offset = parseInt(document.getElementById('offset').value);
+  const result = window.cipher.encode(stringCode, offset);
+   document.getElementById('result').innerHTML = result;
+
+})
+code.addEventListener('click' , () =>{
+  let stringEncode = document.getElementById('textDecipher');
+  let offset = parseInt(document.getElementById('offset').value);
+  let result = window.cipher.encode(stringEncode, offset);
+   document.getElementById('result').innerHTML = result;
+
+})*/
